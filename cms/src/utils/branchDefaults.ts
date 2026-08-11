04099680a925b72b8cbda5d4ac0d5939357c92cb
vuthanhvien <vuthanhvien742@gmail.com>
2026-08-11 19:25:32 +0700

    x

--- cms/src/utils/branchDefaults.ts
+++ cms/src/utils/branchDefaults.ts
@@ -1,12 +1,22 @@
+import { getGlobalBranchFilterIds } from "../api"
+
 export interface SelectOptionLike {
   value?: string
   id?: string
 }
 
 export function getFirstOptionValue<T extends SelectOptionLike>(options: T[]) {
+  const selectedBranchIds = getGlobalBranchFilterIds()
+  const selectedBranch = selectedBranchIds.find((branchId) =>
+    options.some((option) => String(option.value || option.id || "") === branchId),
+  )
+  if (selectedBranch) return selectedBranch
   return options[0]?.value || options[0]?.id
 }
 
 export function getFirstLookupValue(lookup?: Record<string, string>) {
+  const selectedBranchIds = getGlobalBranchFilterIds()
+  const selectedBranch = selectedBranchIds.find((branchId) => Boolean(lookup?.[branchId]))
+  if (selectedBranch) return selectedBranch
   return Object.keys(lookup || {})[0]
 }
